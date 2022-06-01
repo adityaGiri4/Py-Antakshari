ad51d4f3e90ea718e65d587610b8ba5625e47df4
adityaGiri4 <102031891+adityaGiri4@users.noreply.github.com>
2022-06-01 15:44:58 +0530

    Add files via upload

---
+++ hangman/main.py
@@ -0,0 +1,15 @@
+'''
+This file was created by Aditya Giri
+Github : adityaGiri4
+
+This program was made from Python Programming, not to be COPIED
+Here you can play antakshari game with computer
+Rules you can find when you start playing
+'''
+
+import hangman
+
+if __name__ == '__main__':
+    hangman.syllable_loop()
+    hangman.analysis()
+
